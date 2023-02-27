98c469336811c0030b63d95d38f127f0385e9e29
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-02-27 15:34:23 +0100

    Fine esercizio + bonus

--- email/js/main.js
+++ email/js/main.js
@@ -1,18 +1,24 @@
-let email= prompt("Inserisci la tua email");
+const myButton = document.querySelector("#btn")
+myButton.addEventListener("click",
+    function () {
+        let email = document.getElementById("myemail").value;
+        document.getElementById("myemail").innerHTML = email;
 
-const emailList = ["pippobaudo@sanremo.com","fedeznonsocantà@leone.com","paolodibala@magica.com", "cameracaffè@digitex.com","duefantagenitori@pippo.com"]
-let puoAccedere= false;
+        const emailList = ["pippobaudo@sanremo.com", "fedeznonsocantà@leone.com", "paolodibala@magica.com", "cameracaffè@digitex.com", "duefantagenitori@pippo.com"]
+        let puoAccedere = false;
+        for (let i = 0; i < emailList.length; i++) {
+            if (emailList[i] == email) {
+                puoAccedere = true;
+            }
 
-for (let i=0; i < emailList.length; i++){
-    if (emailList[i]==email){
-        puoAccedere=true;
-    }
-       
-    }
+        }
 
-    if(puoAccedere==true){
-        console.log("Puoi accedere!")
+        if (puoAccedere == true) {
+            document.getElementById("result").classList.add('green')
+            document.getElementById("result").innerHTML="Puoi accedere!";
 
-    }else{
-        console.log("non puoi accedere!")
-    }
+        } else {
+            document.getElementById("result2").classList.add('red')
+            document.getElementById("result2").innerHTML="Non puoi accedere!";
+        }
+    })
